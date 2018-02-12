d7e502fbf1d65e6c857a7b7bc799002591b1480f
Adam Hirst <adam@ahirst.com>
2018-02-12 21:51:51 +0000

    [ci skip] Added server SSH config

--- index.js
+++ index.js
@@ -2,15 +2,25 @@
 const express = require('express');
 const LocalStrategy = require('passport-local').Strategy;
 const mongoose = require('mongoose');
+const fs = require('fs');
+const https = require('https');
 
 // Express setup
 const app = express();
+const opts = {
+  key: fs.readFileSync('/etc/letsencrypt/live/grant.mhi.io/privkey.pem'),
+  cert: fs.readFileSync('/etc/letsencrypt/live/grant.mhi.io/fullchain.pem')
+}
 
 // View engine
 app.set('view engine', 'ejs');
 
+// Routes
 app.get('/', (req, res) => {
-  res.render('index')
+  res.render('index');
 });
 
-app.listen(80, () => console.log('Server started on port 3000'));
+// Set the app to listen on port 80
+https.createServer(opts, app).listen(443, () => {
+  console.log("Listening on 443");
+});
