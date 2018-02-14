db652157449f7dfa43ed36858d96dc7fc3b79102
Adam Hirst <adam@ahirst.com>
2018-02-14 18:23:11 +0000

    Fixed login err

--- app/authenticate.js
+++ app/authenticate.js
@@ -1,12 +1,10 @@
 // app/authenticate.js
-const password = require('./password.js');
-
 module.exports = (db) => {
   return (email, password, done) => {
     db.model.User.findOne({ email: email }, (err, user) => {
       if (err) return done(err);
       if (!user) return done(null, false, { message: 'Incorrect username.' });
-      password.verifyHash(password, user.password, (err, res) => {
+      require('./password.js').verifyHash(password, user.password, (err, res) => {
         if (err) return done(err);
         if (!res) return done(null, false, { message: 'Incorrect password.' });
         return done(null, user);
