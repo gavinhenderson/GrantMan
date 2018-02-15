aef67928514278cfc867addd44fe86a035dedc66
Adam Hirst <adam@ahirst.com>
2018-02-15 12:50:51 +0000

    Added id to project schema

--- app/models/Project.js
+++ app/models/Project.js
@@ -4,6 +4,10 @@ var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
 module.exports = new Schema({
+	projectId: {
+		type: Number,
+		required: true
+	}
 	staff: {
 		type: Array,
 		required: true
