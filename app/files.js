38c8df213a6a53d783bc791d9d6e441d1ffeeaa6
Gavin Henderson <gavin.henderson@hotmail.co.uk>
2018-02-22 13:16:24 +0000

    Homepage works

--- app/files.js
+++ app/files.js
@@ -10,16 +10,50 @@ module.exports = (spreadsheet, doc, objectid, cb) => {
 
   //Only run if spreadsheet != null
   if(spreadsheet){
+
+    //Delete file if it exists
+    if(fs.existsSync("public/files/"+objectid+"/spreadsheet.xls")){
+      fs.unlinkSync("public/files/"+objectid+"/spreadsheet.xls")
+    }
+
+    //save spreadsheet
     spreadsheet.mv("public/files/"+objectid+"/spreadsheet.xls", err => {
-      //only run if doc != null
+      //give callback error
       if(err){
         cb(err);
       }
+
+      //only run if doc is given
       if(doc){
+
+        //delete doc if it exists
+        if(fs.existsSync("public/files/"+objectid+"/brief.doc")){
+          fs.unlinkSync("public/files/"+objectid+"/brief.doc")
+        }
+
+        //save brief
         doc.mv("public/files/"+objectid+"/brief.doc", err => {
           cb(err);
         });
+      }else{
+        cb();
       }
     });
+  }else{
+    //only run if doc is given
+    if(doc){
+
+      //delete doc if it exists
+      if(fs.existsSync("public/files/"+objectid+"/brief.doc")){
+        fs.unlinkSync("public/files/"+objectid+"/brief.doc")
+      }
+
+      //save brief
+      doc.mv("public/files/"+objectid+"/brief.doc", err => {
+        cb(err);
+      });
+    }else{
+      cb();
+    }
   }
 }
