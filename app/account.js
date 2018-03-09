af31dd605d59f926b140685f884e5109248873b3
mmcjannett <mitchell.mcjannett@btinternet.com>
2018-03-09 15:46:29 +0000

    Changing the password implementation has began.

--- app/account.js
+++ app/account.js
@@ -1,26 +1,53 @@
 // app/account.js
-module.exports = {
-	createUser : function(db, userDetails, cb){
-		require("./password.js").generateHash(userDetails.password, (err, hash) => {
-			if (err) {
-				cb(err);
-				return;
-			}
-			var newUsr = new db.model.User({
-				staffID: userDetails.id,
-				password: hash,
-				email: userDetails.email,
-				type: userDetails.type,
-				name: userDetails.name,
-				school: userDetails.school
-			});
-			newUsr.validate((err) => {
-				if (err) return cb(err);
+module.exports = (db) => {
+	return {
+		createUser : function(userDetails, cb){
+			require("./password.js").generateHash(userDetails.password, (err, hash) => {
+				if (err) {
+					cb(err);
+					return;
+				}
+				var newUsr = new db.model.User({
+					staffID: userDetails.id,
+					password: hash,
+					email: userDetails.email,
+					type: userDetails.type,
+					name: userDetails.name,
+					school: userDetails.school
+				});
+				newUsr.validate((err) => {
+					if (err) return cb(err);
 
-				newUsr.save((err,user) => {
-					cb(err, user);
+					newUsr.save((err,user) => {
+						cb(err, user);
+					});
 				});
 			});
-		});
+		},
+
+		changePassword : function(req, cb){
+			require("./password.js").verifyHash(req.body.oldPassword, req.user.password, (err, res) => {
+				if (err){
+					cb(err);
+					return;
+				}
+				if(!res){
+					cb(res);
+					return;
+				}
+				else{
+					require("./password.js").generateHash(req.body.newPassword, (err, res) => {
+						if(err){
+							cb(err);
+							return;
+						}
+					});
+				}
+			});
+
+			console.log(req.user);
+			console.log(req.body);
+			cb()
+		}
 	}
 };
