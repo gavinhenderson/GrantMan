f0cd1c219956a8f9e75a3bfee54a6404f5ff8a9b
Gavin Henderson <gavin.henderson@hotmail.co.uk>
2018-03-13 17:46:07 +0000

    merges

--- app/account.js
+++ app/account.js
@@ -25,13 +25,8 @@ module.exports = (db) => {
 			});
 		},
 
-<<<<<<< HEAD
-		changePassword : function(req, cb){
-			require("./password.js").verifyHash(req.body.oldPassword, req.user.password, (err, res) => {
-=======
 		changePassword : function(body, user, cb){
 			require("./password.js").verifyHash(body.oldPassword, user.password, (err, res) => {
->>>>>>> cb41d0085639429359c23f537922b18c4089a96a
 				if (err){
 					cb(err);
 					return;
@@ -41,31 +36,17 @@ module.exports = (db) => {
 					return;
 				}
 				else{
-<<<<<<< HEAD
-					require("./password.js").generateHash(req.body.newPassword, (err, res) => {
-=======
 					require("./password.js").generateHash(body.newPassword, (err, res) => {
->>>>>>> cb41d0085639429359c23f537922b18c4089a96a
 						if(err){
 							cb(err);
 							return;
 						}
-<<<<<<< HEAD
-					});
-				}
-			});
-
-			console.log(req.user);
-			console.log(req.body);
-			cb()
-=======
 						user.password = res;
 						user.save();
 						cb(true);
 					});
 				}
 			});
->>>>>>> cb41d0085639429359c23f537922b18c4089a96a
 		}
 	}
 };
