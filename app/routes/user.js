b5f723b5b31de620a09d59123ab995d0655865a8
Conor Haining <conor.haining@gmail.com>
2018-03-15 11:54:24 +0000

    Creates users for each occurence which has been submitted

--- app/routes/user.js
+++ app/routes/user.js
@@ -37,10 +37,30 @@ module.exports = (app, project, passport, account) => {
 
   //Create account logic
 	app.post("/createaccount", (req,res) => {
-		account.createUser(req.body, (err) => {
-			if (err) { res.send(err); return; }
-			res.redirect("/");
-		});
+		
+		var emails = req.body.email;
+		var types = req.body.type;
+		var schools = req.body.school;
+
+		// res.send(req.body);
+
+		for (var i = emails.length - 1; i >= 0; i--) {
+			
+			account.createUserWithToken(emails[i], types[i], schools[i], (err, user) => {
+				if(err) console.log(err);
+
+				user.save();
+
+				console.log(user);
+
+				//mail.sendEmail("New GrantMan Account", user.email, "NewAccountEmail", user, () => {});
+
+			});
+
+		}
+
+		res.redirect("/");
+
 	});
 
   //Log out and redirect to login
